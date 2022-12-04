e9a54d6ee43341433277d2d45c6a852cef930202
Evan Schaeffer <eschaeffer17@gmail.com>
2022-12-04 15:10:13 -0500

    Day 4

---
+++ 2022/code/4-1.py
@@ -0,0 +1,26 @@
+Day = '4-1'
+
+from pathlib import Path
+
+input_path = "../inputs/"+Day+".txt"
+path = Path(__file__).parent / input_path
+
+with path.open() as f:
+    aoc_input = f.read().splitlines()
+
+result = []
+for i in aoc_input:
+    j = i.split(',')
+    rng1 = j[0].split('-')
+    rng1 = range(int(rng1[0]),int(rng1[1])+1)
+
+    rng2 = j[1].split('-')
+    rng2 = range(int(rng2[0]),int(rng2[1])+1)
+
+    same_ids = range(max(rng1[0], rng2[0]), min(rng1[-1], rng2[-1])+1)
+    
+    full_overlap = (rng1 == same_ids or rng2 == same_ids)
+    result.append(full_overlap)
+
+answer = result.count(True)
+print(answer)
